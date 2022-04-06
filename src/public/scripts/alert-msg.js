7eaa5579dc2060b45779f36c657e58f97fe04739
Michael <michael.doug@outlook.com>
2022-04-05 22:29:40 -0300

    Add static files

---
+++ src/public/scripts/alert-msg.js
@@ -0,0 +1,28 @@
+const toast = document.querySelector(".toast")
+const closeIcon = document.querySelector(".close")
+const progress = document.querySelector(".progress")
+
+if (toast !== null || closeIcon !== null || progress !== null) {
+    
+    let timer1, timer2;
+
+    timer1 = setTimeout(() => {
+        toast.classList.remove("active");
+    }, 5000); //1s = 1000 milliseconds
+
+    timer2 = setTimeout(() => {
+        progress.classList.remove("active");
+    }, 5300);
+
+
+    closeIcon.addEventListener("click", () => {
+        toast.classList.remove("active");
+
+        setTimeout(() => {
+            progress.classList.remove("active");
+        }, 300);
+
+        clearTimeout(timer1);
+        clearTimeout(timer2);
+    })
+}
